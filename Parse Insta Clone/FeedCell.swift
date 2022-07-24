d10fbcf89bf3076e2c07f696b15b15870d6eaa49
Bilalyalcin0 <bilalyalcin0@outlook.com>
2022-07-24 20:34:39 +0300

    first commit

---
+++ Parse Insta Clone/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  Parse Insta Clone
+//
+//  Created by Bilal Yalcin on 31.05.2022.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
